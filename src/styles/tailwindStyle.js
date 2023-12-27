0ff442e7c332b3009980098751f14bf1db597ce0
vietbachtran-202 <vietbach.tran@adelaide.edu.au>
2023-12-28 00:16:05 +1030

    update font & navbar

---
+++ src/styles/tailwindStyle.js
@@ -0,0 +1,16 @@
+const styles = {
+    smText: 'text-xxs  sm:text-xs   md:text-sm   lg:text-base',
+    mdText: 'text-xs   sm:text-sm   md:text-base lg:text-lg',
+    lgText: 'text-sm   sm:text-base md:text-lg   lg:text-xl',
+    xlText: 'text-base sm:text-lg   md:text-xl   lg:text-2xl',
+    xl2Text:'text-lg   sm:text-xl   md:text-2xl  lg:text-3xl',
+    xl3Text:'text-xl   sm:text-2xl  md:text-3xl  lg:text-4xl',
+    xl4Text:'text-2xl  sm:text-3xl  md:text-4xl  lg:text-5xl',
+    navbarPadding: 'px-8 md:px-16 py-8 md:py-8',
+    mainPadding: 'px-8 md:px-20 lg:px-32 py-2 sm:py-8 md:py-4',
+    blogPadding: 'px-8 md:px-20 lg:px-48 py-2 sm:py-8 md:py-4',
+    mainGap: 'gap-4 md:gap-6 lg:gap-12',
+    postListGap: 'gap-x-4 gap-y-4 lg:gap-x-12 lg:gap-y-8'
+};
+  
+export { styles };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				'heading': ['"Alegreya Sans SC"', defaultTheme.fontFamily.sans],
+				'body'    : ['Poppins', defaultTheme.fontFamily.serif],
+			},
 			colors: {
 				'text': '#ffffff',
 				'background': '#193c51',
