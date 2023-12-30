931af07d7408ccfe9869269d6656cc9579b4ba68
vietbachtran-202 <vietbach.tran@adelaide.edu.au>
2023-12-30 21:08:08 +1030

    an update

--- src/styles/tailwindStyle.js
+++ src/styles/tailwindStyle.js
@@ -6,7 +6,7 @@ const styles = {
     xl2Text:'text-xl   lg:text-2xl',
     xl3Text:'text-2xl   lg:text-3xl',
     xl4Text:'text-3xl  lg:text-4xl',
-    navbarPadding: 'px-8 md:px-16 py-8 md:py-8',
+    navbarPadding: 'px-3 md:px-8 lg:px-16 py-4 md:py-8',
     footerPadding: 'px-8 md:px-16 pt-8 pb-4 md:pt-12 md:pb-4',
     mainPadding: 'px-8 md:px-20 lg:px-32 py-2 sm:py-8 md:py-4',
     colAUMSPadding: 'px-8 md:px-20 lg:px-32 py-2 sm:py-8 md:py-12',
